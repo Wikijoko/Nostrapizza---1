778f8adac9c524227bbec125627bd438242c91ba
Wikijoko <98783004+Wikijoko@users.noreply.github.com>
2022-11-02 12:53:07 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 let delivery1 = document.querySelector(".delivery-container")
 let deliveryboton = document.querySelector(".delivery-box")
+let entradas = document.querySelector(".entradas")
+let menu = document.querySelector(".menu-container")
+let menuentradas = document.querySelector(".entradasmenu")
+let volver = document.querySelector(".entradasvolver")
 
     deliveryboton.addEventListener('click', () => {
     delivery1.classList.toggle('activo')
 })
+
+    entradas.addEventListener('click', () => {
+    menu.style.display="none";
+    menuentradas.style.display="block";
+})
+    volver.addEventListener('click', () => {
+    menuentradas.style.display="none";
+    menu.style.display="block";
+    })
